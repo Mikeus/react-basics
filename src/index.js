56ddbf3eaf7476f94d1466125681f43752eae1e0
Michal Ziembicki <vipmikeus@gmail.com>
2022-03-11 17:20:09 -0500

    END  - props children

--- src/index.js
+++ src/index.js
@@ -4,19 +4,32 @@ import ReactDOM from 'react-dom'
 const App = () => {
   return (
     <>
-      <Header background='red'>Logo</Header>
+      <Header background='red'>
+        <nav>
+          <a href='#'>Link 1</a>
+          <a href='#'>Link 1</a>
+          <a href='#'>Link 1</a>
+        </nav>
+        Logo
+      </Header>
       <Header background='blue'>Logo</Header>
-      <Header>Logo3</Header>
+      <Header>
+        <Logo>Logo3eee</Logo>
+      </Header>
     </>
   )
 }
+const Logo = (props) => {
+  return <>{props.children}</>
+}
+
 const Header = (props) => {
   return (
     <header
       style={{
         background:
           props.background === undefined ? 'yellow' : props.background,
-        color: 'white',
+        color: 'green',
         height: '100px',
       }}
     >
