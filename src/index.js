6a1fb1c08423f41b6373aa774963626c2f0a0463
Michal Ziembicki <vipmikeus@gmail.com>
2022-03-11 17:13:45 -0500

    END- props children

--- src/index.js
+++ src/index.js
@@ -1,60 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let name = 'john'
-let lname = 'santos'
-let age = 21
-let logedIn = true
-let width = 600
-let styles = {
-  box: {
-    width: `${(width * 3) / 2}px`,
-    height: '300px',
-    display: 'flex',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-}
-
-const App = (props) => {
-  // console.log(props)
-  const printAlert = (message) => {
-    alert(message)
-  }
-  let showAccount = () => {
-    return <div>You are log in.</div>
-  }
+const App = () => {
   return (
-    <div>
-      <h1
-        style={{
-          background: 'red',
-          color: 'white',
-          textAlign: 'center',
-        }}
-      >
-        Welcome {name + ' ' + lname}
-      </h1>
-      <h2>You are {age * 2} years old.</h2>
-      {logedIn ? showAccount() : ''}
-
-      {/* <h3 onClick={() => props.printAlert('Hello Alert')}>Text H3</h3> */}
-
-      <Heading
-        content='text content'
-        periods={['', '.', '..', '...']}
-        printAlert={printAlert}
-      ></Heading>
-      {/* <Heading content='text2 content'></Heading> */}
-    </div>
+    <>
+      <Header background='red'>Logo</Header>
+      <Header background='blue'>Logo</Header>
+      <Header>Logo3</Header>
+    </>
   )
 }
-const Heading = (props) => {
-  console.log(props)
+const Header = (props) => {
   return (
-    <h1 onClick={props.printAlert.bind(null, 'HI WORLD')}>
-      {props.content} {props.periods === undefined ? '' : props.periods[3]}
-    </h1>
+    <header
+      style={{
+        background:
+          props.background === undefined ? 'yellow' : props.background,
+        color: 'white',
+        height: '100px',
+      }}
+    >
+      {props.children}
+    </header>
   )
 }
 
